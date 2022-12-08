2ceb588b0c7c3a6d76c954dc6f8f62a894c74942
unknown <trinhnam180697@gmail.com>
2022-12-08 10:37:11 -0500

    ToanTu

---
+++ SuDungToanTu/ToanTu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth=prompt("Nhập vào chiều rộng : ");
+inputHeight=prompt("Nhập vào chiều dài : ");
+let width = parseInt(inputWidth);
+let Height = parseInt(inputHeight);
+let area = width * Height;
+document.write("Diện tích là : " + area);
